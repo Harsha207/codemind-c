9315893df6f3bdce78a11381f279c3666be65d90
Harsha207 <96769041+Harsha207@users.noreply.github.com>
2022-01-25 14:45:41 +0530

    #include<stdio.h> int main() {     int a,b,i,c;     scanf("%d%d",&a,&b);     for(i=1;i<=b;i++)     {         if(i%2==0)         {             continue;         }         c=a*i;         printf("%d x %d = %d ",a,i,c);     } }

---
+++ Tables.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+int main()
+{
+    int a,b,i,c;
+    scanf("%d%d",&a,&b);
+    for(i=1;i<=b;i++)
+    {
+        if(i%2==0)
+        {
+            continue;
+        }
+        c=a*i;
+        printf("%d x %d = %d
+",a,i,c);
+    }
+}
