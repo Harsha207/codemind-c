5ae2464efdd3730164b1093f2bbfbab3fd2cb38e
Harsha207 <96769041+Harsha207@users.noreply.github.com>
2022-02-17 13:21:15 +0530

    #include<stdio.h> int main() {     int n,r,a,sum=0;     scanf("%d",&n);     a=n;     while(n>0)     {         r=n%10;         sum=sum+r;         n=n/10;     }     n=a;     if(a%sum==0)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Harshed_number.c
@@ -0,0 +1,23 @@
+
+#include<stdio.h>
+int main()
+{
+    int n,r,a,sum=0;
+    scanf("%d",&n);
+    a=n;
+    while(n>0)
+    {
+        r=n%10;
+        sum=sum+r;
+        n=n/10;
+    }
+    n=a;
+    if(a%sum==0)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
