c671ab90ec24693e1ec900a383de732ab7931aa7
Harsha207 <96769041+Harsha207@users.noreply.github.com>
2022-02-22 14:41:09 +0530

    #include<stdio.h> int main() {     int n,i,a[100],count=0;     scanf("%d",&n);     a[0]=0;     a[1]=1;     for(i=1;i<n+1;i++)     {         a[i+1]=a[i]+a[i-1];     }     for(i=0;i<n;i++)     {         printf("%d ",a[i]);     } }

---
+++ fibonacci.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+int main()
+{
+    int n,i,a[100],count=0;
+    scanf("%d",&n);
+    a[0]=0;
+    a[1]=1;
+    for(i=1;i<n+1;i++)
+    {
+        a[i+1]=a[i]+a[i-1];
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+}
+
