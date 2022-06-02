d2e84af7556e3bc11f42066c3297dc5972ad5044
Harsha207 <96769041+Harsha207@users.noreply.github.com>
2022-06-02 14:57:34 +0530

    #include<stdio.h> int main() {     int n,i,j,arr[100],s=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         for(j=i+1;j<n;j++)         {             if(arr[i]==arr[j])             {                 arr[j]=1;             }         }         if(arr[i]%2==0 && arr[i]!=1)         {             s=s+arr[i];         }     }     printf("%d",s); }

---
+++ unique_even_elements_sum.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,arr[100],s=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(arr[i]==arr[j])
+            {
+                arr[j]=1;
+            }
+        }
+        if(arr[i]%2==0 && arr[i]!=1)
+        {
+            s=s+arr[i];
+        }
+    }
+    printf("%d",s);
+}
