09910cc7e4c9791274bc7d203b3fe264b871f1d2
Harsha207 <96769041+Harsha207@users.noreply.github.com>
2022-06-28 20:12:05 +0530

    #include<stdio.h> int main() {     int n,i,arr[100],s=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         s=s+arr[i];     }     printf("%d",s); }

--- Sum_of_Array.c
+++ Sum_of_Array.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,s=0,arr[100];
+    int n,i,arr[100],s=0;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
         scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
         s=s+arr[i];
     }
     printf("%d",s);
