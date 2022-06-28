d4a4dfb6812bdd7f76aadaea367061ae058070c4
Harsha207 <96769041+Harsha207@users.noreply.github.com>
2022-06-28 19:59:50 +0530

    #include<stdio.h> int main() {     int n,i,s=0,arr[100];     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(i%2!=0)         {             s=s+arr[i];         }     }     printf("%d",s); }

--- Sum_of_Odd_Positions.c
+++ Sum_of_Odd_Positions.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,arr[100],s=0;
+    int n,i,s=0,arr[100];
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
         scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
         if(i%2!=0)
         {
             s=s+arr[i];
