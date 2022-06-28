5d47f3cf290472b8c9259f380a4b87ab86e71ea2
Harsha207 <96769041+Harsha207@users.noreply.github.com>
2022-06-28 22:06:34 +0530

    #include<stdio.h> int main() {     int n,m,i,j,s=0;     int arr[100][100];     scanf("%d%d",&n,&m);     for(i=1;i<=n;i++)     {         for(j=1;j<=m;j++)         {             scanf("%d",&arr[i][j]);         }     }     for(i=1;i<=n;i++)     {         for(j=1;j<=m;j++)         {             if(i==1 || i==n || j==1 || j==m)             {                 s=s+arr[i][j];             }         }     }     printf("%d",s); }

--- Sum_of_all_boundary_values_in_a_matrix.c
+++ Sum_of_all_boundary_values_in_a_matrix.c
@@ -1,23 +1,23 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j,m,s=0;
-    int mat[10][10];
+    int n,m,i,j,s=0;
+    int arr[100][100];
     scanf("%d%d",&n,&m);
     for(i=1;i<=n;i++)
     {
         for(j=1;j<=m;j++)
         {
-            scanf("%d",&mat[i][j]);
+            scanf("%d",&arr[i][j]);
         }
     }
     for(i=1;i<=n;i++)
     {
         for(j=1;j<=m;j++)
         {
-            if(i==1||j==1||i==n||j==m)
+            if(i==1 || i==n || j==1 || j==m)
             {
-                s=s+mat[i][j];
+                s=s+arr[i][j];
             }
         }
     }
